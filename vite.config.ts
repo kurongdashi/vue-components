c2b84ab7630c17a3c4e6cb95972906c92dffb41a
lp123 <1970664102@qq.com>
2024-01-06 23:03:15 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+
+  build: {
+    lib: {
+      // 入口
+      entry: fileURLToPath(new URL('./src/index.js', import.meta.url)),
+      name: 'lib',
+      fileName: 'lib'
+    },
+    rollupOptions: {
+      // 不需要打包进库的依赖，忽略vue
+      external: ['vue'],
+      output: {
+        // UMD构建模式下为这些外部的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
